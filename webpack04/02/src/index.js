c70635f17b8ee0a57a538f4000703f3346c90b27
LiangJieMr <13453796194@163.com>
2020-10-10 11:13:27 +0800

    选项卡

--- webpack04/02/src/index.js
+++ webpack04/02/src/index.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: liangJie
+ * @Date: 2020-09-24 16:02:34
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2020-09-29 11:27:08
+ * @FilePath: \02\src\index.js
+ * @Description: 
+ */
 // 模块化开发 当前流行的单页面入口应用 spa
 /**
  * 引入模块 形成依赖
@@ -9,7 +17,7 @@
 // import { add } from "./other.js"; // es module
 import 'font-awesome/css/font-awesome.css'
 import css from "./css/index.less";
-// import pic from "./image/logo.jpg";
+import pic from "./image/logo.jpg";
 
 // let ele = `<div class=${css.ele}>css module</div>`;
 console.log("123")
